035785463d276f3dfe2e8eb65034a40745d98e7b
sweanan <swethaanand@microsft.com>
2023-01-30 16:05:39 -0600

    updated files

--- scripts/pipeline/createFluxGotk.sh
+++ scripts/pipeline/createFluxGotk.sh
@@ -3,13 +3,14 @@ echo "createFluxGotk.sh"
 
 for cluster in clusters/*.yaml; do
     echo "cluster: $cluster"
+    # cluster: clusters/dev.yaml
     cluster_name=$(yq '.metadata.labels.aksClusterName' "$cluster")
     cluster_rg=$(yq '.metadata.labels.aksClusterResourceGroup' "$cluster")
 
     mkdir -p clusters/dev/flux-system
     if [[ -f clusters/dev/flux-system/gotk-components.yaml ]] || [[ -f clusters/dev/flux-system/gotk-sync.yaml ]] || [[ -f clusters/dev/flux-system/kustomization.yaml ]]; then exit 0; fi
     echo "flux kustomization files not fund, creating..."
-    touch clusters/$cluster/flux-system/gotk-components.yaml \    
-        clusters/$cluster/flux-system/gotk-sync.yaml \
-        clusters/$cluster/flux-system/kustomization.yaml
+    touch clusters/dev/flux-system/gotk-components.yaml \    
+        clusters/dev/flux-system/gotk-sync.yaml \
+        clusters/dev/flux-system/kustomization.yaml
 done

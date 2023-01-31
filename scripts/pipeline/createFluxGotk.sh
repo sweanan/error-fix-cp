56e7a352991c4e60b78fc67f6a6fd93b1fa8f019
sweanan <swethaanand@microsft.com>
2023-01-31 10:15:31 -0600

    clean up

--- scripts/pipeline/createFluxGotk.sh
+++ scripts/pipeline/createFluxGotk.sh
@@ -3,9 +3,6 @@ echo "createFluxGotk.sh"
 
 for cluster in clusters/*/; do
     echo "cluster: $cluster"
-    # cluster: clusters/dev.yaml
-    # cluster_name=$(yq '.metadata.labels.aksClusterName' "$cluster")
-    # echo "cluster_name: $cluster_name"
 
     mkdir -p $cluster/flux-system
     if [[ -f $cluster/flux-system/gotk-components.yaml ]] || [[ -f $cluster/flux-system/gotk-sync.yaml ]] || [[ -f $cluster/flux-system/kustomization.yaml ]]; then exit 0; fi

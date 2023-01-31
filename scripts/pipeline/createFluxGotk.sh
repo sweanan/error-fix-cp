5d53aa10101379a0d4de50b8be4341d622830ab5
sweanan <swethaanand@microsft.com>
2023-01-31 09:58:58 -0600

    update the createFlux

--- scripts/pipeline/createFluxGotk.sh
+++ scripts/pipeline/createFluxGotk.sh
@@ -4,11 +4,13 @@ echo "createFluxGotk.sh"
 for cluster in clusters/*/; do
     echo "cluster: $cluster"
     # cluster: clusters/dev.yaml
-    
-    mkdir -p ./gitops/clusters/$cluster/flux-system
-    if [[ -f ./gitops/clusters/$cluster/flux-system/gotk-components.yaml ]] || [[ -f ./gitops/clusters/$cluster/flux-system/gotk-sync.yaml ]] || [[ -f ./gitops/clusters/$cluster/flux-system/kustomization.yaml ]]; then exit 0; fi
+    # cluster_name=$(yq '.metadata.labels.aksClusterName' "$cluster")
+    # echo "cluster_name: $cluster_name"
+
+    mkdir -p $cluster/flux-system
+    if [[ -f $cluster/flux-system/gotk-components.yaml ]] || [[ -f $cluster/flux-system/gotk-sync.yaml ]] || [[ -f $cluster/flux-system/kustomization.yaml ]]; then exit 0; fi
     echo "flux kustomization files not fund, creating..."
-    touch ./gitops/clusters/$cluster/flux-system/gotk-components.yaml 
-    touch ./gitops/clusters/$cluster/flux-system/gotk-sync.yaml
-    touch ./gitops/clusters/$cluster/flux-system/kustomization.yaml
+    touch $cluster/flux-system/gotk-components.yaml 
+    touch $cluster/flux-system/gotk-sync.yaml
+    touch $cluster/flux-system/kustomization.yaml
 done
